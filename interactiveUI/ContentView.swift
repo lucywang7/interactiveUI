926770a852c95a6ec33474bb6779c039d61ba1b3
Lucy <lucywang340@gmail.com>
2025-07-09 14:38:32 -0500

    I put in a user questions

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("What is your name?")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.blue)
         }
-        .padding()
     }
 }
 
