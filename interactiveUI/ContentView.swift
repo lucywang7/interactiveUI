a90b4ba532dc8df8b038625742824cd8a638daf3
Lucy <lucywang340@gmail.com>
2025-07-09 14:33:17 -0500

    Initial Commit

---
+++ interactiveUI/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  interactiveUI
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
