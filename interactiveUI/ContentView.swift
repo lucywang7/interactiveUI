bcbd2e74bf7c422e32e82544508114feabe368dc
Lucy <lucywang340@gmail.com>
2025-07-09 14:56:55 -0500

    text view and button

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -14,7 +14,18 @@ struct ContentView: View {
                 .font(.title)
                 .fontWeight(.semibold)
                 .foregroundColor(Color.blue)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
         }
+        .padding()
     }
 }
 
